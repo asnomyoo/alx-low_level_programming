bcbc2ab4376c00a75098b777887c825e25514e09
asnomyoo <party.glamor123@gmail.com>
2023-09-07 19:09:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+    puts("\"Programming is like building a multilingual puzzle")
+    return (0);
 }
