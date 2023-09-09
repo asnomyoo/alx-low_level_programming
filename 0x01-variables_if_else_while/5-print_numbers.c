74e3b094a8c843c2b722395fe583124972579520
asnomyoo <party.glamor123@gmail.com>
2023-09-08 18:25:55 -0700

    azert

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: print all alphabet lettres except q and e
+*
+* Return: Always 0 (Seccess)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%i", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
