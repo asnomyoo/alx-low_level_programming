1a3de6b99a4f7f57004f94c6744a3dbfa05bc1d7
asnomyoo <party.glamor123@gmail.com>
2023-09-08 17:14:13 -0700

    aze

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - main function
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char i = 'a';
+char I = 'Z';
+
+/*prints a - z*/
+while (i <= 'z')
+{
+putchar(i);
+i++;
+}
+/*prints A - Z*/
+while (I <= 'Z')
+{
+putchar(I);
+I++;
+}
+putchar('\n');
+return (0);
+}
+
