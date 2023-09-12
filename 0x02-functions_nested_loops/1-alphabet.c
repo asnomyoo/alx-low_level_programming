82a9ec4aedfa26dd6f83378a04e18a750397222d
asnomyoo <party.glamor123@gmail.com>
2023-09-12 11:58:47 -0700

    nbv

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - fonction to print alphabet
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z' ; i++)
+_putchar(i);
+_putchar('\n');
+}
