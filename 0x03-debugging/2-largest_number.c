8b002dbdf0304c3abb875d6b9d76df8e15193f57
asnomyoo <party.glamor123@gmail.com>
2023-09-13 16:27:27 -0700

    oui

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* largest_number - program prints the largest of three integers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
+
+int largest_number(int a, int b, int c)
+{
+int largest;
+
+if (a >= b && b >= c)
+{
+largest = a;
+}
+else if (b > a && b >= c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+return (largest);
+}
