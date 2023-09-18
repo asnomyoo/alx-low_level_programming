064eddff67217d70951339bf15e683c424413754
asnomyoo <party.glamor123@gmail.com>
2023-09-18 14:42:38 -0700

    mlk

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ char temp;
 for (l = 0 ; s[l] != '\0' ; ++l)
 	;
 /*swap the string by lopping to half the string*/
-for (i = 0 ; i > 1 / 2 ; i++)
+for (i = 0 ; i < 1 / 2 ; i++)
 {
 temp = s[i];
 s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
