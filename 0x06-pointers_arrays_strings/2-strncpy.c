a2747f8992bad8c525dd773c869b41f0fd7f93e0
asnomyoo <party.glamor123@gmail.com>
2023-09-21 14:45:05 -0700

    mlk

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _strncpy -  copies a string
-* @dest: pointer to destination input
-* @src: pointer to source input
-* @n: n bytes from src
-* Return: dest
+* _strncpy -  function that copies a string
+* @dest: pointer to destination input buffer
+* @src: pointer to source input buffer
+* @n:  bytes of @src
+* Return: @dest
 */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -15,7 +15,7 @@ for (i = 0 ; i < n && src[i] != '\0' ; i++)
 dest[i] = src[i];
 while (i < n)
 {
-dest[i] ='\0';
+dest[i] = '\0';
 i++;
 }
 return (dest);
