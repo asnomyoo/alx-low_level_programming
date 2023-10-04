911e8f5c141eb04680d065e19218e0d0397860d8
asnomyoo <party.glamor123@gmail.com>
2023-10-04 15:00:24 -0700

    mlk

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
 * _strlen - find length of a string
 * @s: string
 * Return: int
 */
+
 int _strlen(char *s)
 {
 int size = 0;
@@ -12,7 +12,6 @@ for (; s[size] != '\0' ; size++)
 ;
 return (size);
 }
-
 
 /**
 * *str_concat - concatenates two strings
