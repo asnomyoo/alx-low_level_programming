30056ace716d4bca7194b7a947e2b540942e61db
asnomyoo <party.glamor123@gmail.com>
2023-10-08 15:16:56 -0700

    mlk

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,26 +12,31 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j;
 char *s;
+unsigned int i, j, len1, len2;
+len1 = strlen(s1);
+len2 = strlen(s2);
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-if (n >= strlen(s2))
-s = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-else
-s = malloc(sizeof(char) * (strlen(s1) + n + 1));
+for (len1 = 0 ; s1[len1] != '\0' ; len1++)
+;
+
+for (len2 = 0; s2[len2] != '\0'; len2++)
+;
+s = malloc(len1 + n + 1);
 if (s == NULL)
+{
 return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-{
-s[i] = s1[i];
 }
-for (j = 0; j < n && s2[j] != '\0'; j++)
+for (i = 0 ; s1[i] != '\0' ; i++)
+s[i] = s1[i];
+for (j = 0 ; j < n ; j++)
 {
-s[i] = s2[j];
+s[i] = s[j];
 i++;
 }
+s[i] = '\0';
 return (s);
 }
