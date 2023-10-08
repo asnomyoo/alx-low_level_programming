02f14787dcecb45c2854b84216c9940cd3d74cd2
asnomyoo <party.glamor123@gmail.com>
2023-10-08 14:23:23 -0700

    mlk

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* *_memset - fills memory with a cst byte
+*@s: pointer to put the cst
+*@b: cst
+*@n: max bytes to use
+* Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *ptr = s;
+while (n--)
+*s++ = b;
+return (ptr);
+}
+
+/**
+ * *_calloc - allocates memory for an array, using malloc.
+ * @nmemb: array length
+ * @size: size of element
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *m;
+if (size == 0 || nmemb == 0)
+return (NULL);
+m = malloc(sizeof(int) * nmemb);
+if (m == 0)
+return (NULL);
+_memset(m, 0, sizeof(int) * nmemb);
+return (m);
+}
